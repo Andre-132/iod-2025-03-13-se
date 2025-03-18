1abcf8fbb75f890e7a84032550350691cc8888a9
Andre Albarado <analbarado1@gmail.com>
2025-03-18 07:48:50 -0500

    helloworld.js

---
+++ Module1/ClassExercise/helloworld.js
@@ -0,0 +1,23 @@
+//console.log("hello world");
+
+//function sayHello() {
+    //console.log("Hello");
+ //}
+
+ // this is to call the function
+ //sayHello();
+
+ //function sum(a, b) {
+   // console.log(a + b);
+    //return a + b;
+ //}
+
+ //sum(5, 1);
+
+ //ABOVE IS FOR SCHOOL!!!!
+
+ console.log("Hello World");
+ 
+
+
+
