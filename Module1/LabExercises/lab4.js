37f80090d6d01c5b76803912681217d4aa42acea
Andre Albarado <analbarado1@gmail.com>
2025-03-19 19:14:33 -0500

    Lab Exercise 4

---
+++ Module1/LabExercises/lab4.js
@@ -0,0 +1,35 @@
+function sum(a, b) {
+    let result= a + b;
+    console.log(result)
+}
+sum(3, 4);//logs 7
+sum(10, 5);//logs 15
+
+function difference(a, b) {
+    let result= a - b;
+    console.log(result)
+}
+difference(5, 3);//logs 2
+difference(7, 2);//logs 5
+
+function multiply(a, b) {
+    let result= a * b; 
+    console.log(result)
+}
+multiply(5, 5);//logs 25
+multiply(3, 2);//logs 6
+
+function divide(a, b) {
+    let result= a / b;
+    console.log(result)
+}
+divide(8, 2);//logs 4
+divide(10, 2);//logs 5
+
+function sayHello(message) {
+    console.log(message); 
+}
+
+sayHello("Hello Andre"); 
+
+
