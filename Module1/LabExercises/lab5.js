00373bf5d4139532ef1723a8b9938266c18b69bc
Andre Albarado <analbarado1@gmail.com>
2025-03-19 19:42:21 -0500

    Lab Exercise 5

---
+++ Module1/LabExercises/lab5.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "orange", "kiwi", "strawberry", "blueberry"];
+
+    fruits[1] = "elderberry";
+    fruits[4] = "plum";
+
+    fruits.unshift("mango");
+    fruits.pop();
+
+    console.log(fruits);
