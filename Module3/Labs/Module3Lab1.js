e63d39df4bfe0d9b41a95ed673e1892e9bc9e46f
Andre Albarado <analbarado1@gmail.com>
2025-04-07 21:02:56 -0500

    First Commit

---
+++ Module3/Labs/Module3Lab1.js
@@ -0,0 +1,32 @@
+console.log("" + 1 + 0); // This equals 10 because empty string has the ability to combine two numbers rather than add them and in JavaScript "number" + number equals the two numbers put beside each other.
+
+console.log("" - 1 + 0); // This equals -1 reason being is because the string takes over -1 and the 0 plays no role since the string takes favor of -1
+
+console.log(true + false); // This equals 1 because in JavaScript True is of 1 and False is of 0 which means 1 + 0 = 1
+
+console.log(!true); // This equals False and the reason being is because the expression "!" in boolean logic means the opposite of what is displayed for example !true = False and !false = True.
+
+console.log(6 / "3"); // This equals 2 because in JavaScript the "/" symbol means division.
+
+console.log("2" * "3"); // This equals 6 because the "*" means multiplication and 2 times 3 equals 6.
+
+console.log(4 + 5 + "px"); // This equals 9px and that's because the string "px" is adding to pixels attribute to the solution 4 + 5 which equals 9.
+
+console.log("$" + 4 + 5); // This equals 45$
+
+"4" - 2;
+
+"4px" - 2;
+
+" -9 " + 5;
+
+" -9 " - 5;
+
+null + 1;
+
+undefined + 1;
+
+undefined == null;
+
+undefined === null;
+" \t \n" - 2;
