586569178daf689cddb5a095ab1580e575a98e3e
Andre Albarado <analbarado1@gmail.com>
2025-04-27 17:38:42 -0500

    minor fixes needed

--- Module4/Labs/Lab2/calc.js
+++ Module4/Labs/Lab2/calc.js
@@ -7,7 +7,7 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.textContent;
 
-    if (value === "clear") {
+    if (value === "Clear") {
       clearDisplay();
     } else if (value === "=") {
       calculateResult();
@@ -24,11 +24,39 @@ function appendValue(value) {
 
 function clearDisplay() {
   currentInput = "";
-  display.value = ""; 
+  display.value = "";
 }
 
 function calculateResult() {
   if (currentInput === "") return;
 
-   let numbers = currentInput.split(/[\+\-\*\/]/g).map(Number);
-   let operators = currentInput.replace(/[0-9\.]/g, "").split("");
+  let numbers = currentInput.split(/[\+\-\*\/]/g).map(Number);
+  let operators = currentInput.replace(/[0-9\.]/g, "").split("");
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "*" || operators[i] === "/") {
+      let result =
+        operators[i] === "*"
+          ? numbers[i] * numbers[i + 1]
+          : numbers[i] / numbers[i + 1];
+      numbers.splice(i, 2, result);
+      operators.splice(i, 1);
+      i--;
+    }
+  }
+
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "+" || operators[i] === "-") {
+      let result =
+        operators[i] === "+"
+          ? numbers[i] + numbers[i + 1]
+          : numbers[i] - numbers[i + 1];
+      numbers.splice(i, 2, result);
+      operators.splice(i, 1);
+      i--;
+    }
+  }
+
+  currentInput = numbers[0].toString();
+  display.value = currentInput;
+}
