0e8286dded2885750a9516c3b548abe7e99ca981
Andre Albarado <analbarado1@gmail.com>
2025-04-27 17:05:43 -0500

    added if else & functions

--- Module4/Labs/Lab2/calc.js
+++ Module4/Labs/Lab2/calc.js
@@ -5,6 +5,30 @@ let currentInput = "";
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const value = button.innerText;
+    const value = button.textContent;
+
+    if (value === "clear") {
+      clearDisplay();
+    } else if (value === "=") {
+      calculateResult();
+    } else {
+      appendValue();
+    }
   });
 });
+
+function appendValue(value) {
+  currentInput += value;
+  display.value = currentInput;
+}
+
+function clearDisplay() {
+  currentInput = "";
+  display.value = ""; 
+}
+
+function calculateResult() {
+  if (currentInput === "") return;
+
+   let numbers = currentInput.split(/[\+\-\*\/]/g).map(Number);
+   let operators = currentInput.replace(/[0-9\.]/g, "").split("");
