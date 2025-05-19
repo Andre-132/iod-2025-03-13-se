c6f816ba51234d34edb917ec1ea8c5d89bb7934e
Andre Albarado <analbarado1@gmail.com>
2025-05-18 21:44:20 -0500

    Lab1 Complete

--- Module5/express/index.js
+++ Module5/express/index.js
@@ -1,12 +1,28 @@
+// Lab 1
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("This is running on port 3000");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening
 at http://localhost:${port}`);
 });
+
+const app2 = express();
+const port2 = 4000;
+
+app2.get("/", (req, res) => {
+  res.send("This is running on port 4000");
+});
+
+app2.listen(port2, () => {
+  console.log(`Example app listening
+at http://localhost:${port2}`);
+});
+
+// Lab 2
