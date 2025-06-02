84849d7ac66a73518719209efb154cee692bd678
Andre Albarado <analbarado1@gmail.com>
2025-06-01 19:55:51 -0500

    swagger

--- Module5/express/calculator/index.js
+++ Module5/express/calculator/index.js
@@ -1,13 +1,18 @@
 const path = require("path");
 const express = require("express");
-const appController = require("./routes/lab2");
+const { router: appController } = require("./routes/lab2");
 const port = 3000;
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/calculator", appController);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`app server running at http://localhost:${port}`);
